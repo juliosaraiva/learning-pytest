5cc2eac7560cb1b7c162d6a3ddffee2f65ea2624
Julio Saraiva <contato@juliosaraiva.com.br>
2021-03-15 00:37:09 -0300

    feat: create project

---
+++ src/core/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from core.models import Company
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    pass
+
+
+
+
+
+
---
+++ src/core/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Company(models.Model):
+
+    class CompanyStatus(models.TextChoices):
+        LAYOFFS = 'Layoffs'
+        HIRING_FREEZE = 'Hiring Freeze'
+        HIRING = 'Hiring'
+
+    name = models.CharField(max_length=20, unique=True)
+    status = models.CharField(choices=CompanyStatus.choices, max_length=20, default=CompanyStatus.HIRING)
+    url = models.URLField(blank=True)
+    notes = models.CharField(max_length=100, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'company'
+        verbose_name_plural = 'companies'
